50f49e1775fdcf1a02f8d6c14269ae13550b73b9
davronbekanvarov23 <davronbekanvarov23@gmail.com>
2024-02-24 15:12:03 +0500

    completed

---
+++ app.js
@@ -0,0 +1,158 @@
+// 3-VIDEO
+
+// alert('Salom dunyo')
+// alert('mening ismim Davronbek')
+
+// 4-VIDEO
+
+// let ism = "Davronbek";
+// console.log(ism);
+
+// let familiya = "Anvarov";
+// console.log(familiya);
+
+// let yosh = "22";
+// console.log(yosh);
+
+// let 1StartsWithNumber='startsWithNumber'
+// let with space ='withspace'
+// let other@symbolused='otherSymbolUsed'
+// let for='Reserved word'
+
+// 5-VIDEO
+
+// Number
+// let age = 22;
+// let price = 19.99;
+// console.log(age);
+// console.log(price);
+
+// String
+// let firthName='Davronbek'
+// console.log(firthName)
+
+// boolean
+// let isUser = true;
+// console.log(isUser);
+
+// Undefined
+// let student;
+// console.log(student)
+
+// Null
+// let school=null
+// console.log(school)
+
+// Symbol
+// let id = Symbol("id");
+// console.log(id);
+
+// // BigInt
+// const alsoHuge = BigInt(90009748513218541321);
+// console.log(alsoHuge);
+
+// let age = 22;
+// console.log(typeof age)
+
+// 6-VIDEO
+
+// let yosh = 22;
+// console.log(yosh);
+
+// yosh = 23;
+// console.log(yosh);
+
+// const id = "AA0";
+// console.log(id);
+// id = "AA1";
+
+// var yosh = 22;
+// console.log(yosh);
+// yosh = 23;
+// console.log(yosh)
+
+// 8-VIDEO
+
+// let ism='Ulug\'bek'
+// console.log(ism)
+
+// let familiya="Samig\"jonov"
+// console.log(familiya)
+
+// let ism = "Davronbek";
+// let message = "salom " + ism + "!";
+// console.log(message);
+
+// let ism='Davronbek'
+// let message=`salom ${ism} !`
+// console.log(message);
+
+// 9-VIDEO
+
+// const yosh=10
+// const a=yosh>18 //false
+// const b=yosh<20 //true
+
+// //AND operatori
+// // console.log(a&&b)
+
+// // OR operatori
+// console.log(a||b)
+
+// // NOT operatori
+// console.log(!a)
+// console.log(!b)
+
+// const htmlPassed = true;
+// const cssPassed = true;
+// let message = "";
+// if (htmlPassed && cssPassed) {
+//   message = "Siz bootstrap kursini boshlashingiz mumkin!";
+// } else if (htmlPassed || cssPassed) {
+//   message = "Iltimos ikkinchi kursni ham tugating!";
+// } else {
+//   message = "Iltimos birinchi ikkala kursni ham tugating!";
+// }
+
+// console.log(message);
+
+// 10-VIDEO
+// const yosh = "22"; //string
+// console.log(yosh); //string
+// console.log(Number(yosh));//number
+
+// console.log(yosh+1) //221 (concatenation)
+// console.log(Number(yosh)+1) //23
+
+// const ism='Davronbek'
+// console.log(Number(ism))
+
+// console.log('men '+2002+'chi yilda tavallud topganman')
+
+// console.log("30" - "10" - "5");
+
+// Falsy qiymatar
+
+// console.log(Boolean(false));
+// console.log(Boolean(0));
+// console.log(Boolean(""));
+// console.log(Boolean(undefined));
+// console.log(Boolean(null));
+// console.log(Boolean(NaN));
+// console.log(Boolean(0n));
+
+// // truthy qiymatlar
+// console.log(Boolean("davronbek"));
+// console.log(Boolean(22));
+// console.log(Boolean({}));
+// console.log(Boolean([]));
+// console.log(Boolean("0"));
+// console.log(Boolean("false"));
+
+// const ism = "Davronbek";
+// if (ism) {
+//   console.log("sizning ismingiz: " + ism);
+// } else {
+//   console.log("iltimos ism kiriting");
+// }
+
